2cdc1fc870207d6282ee8ff05105530d896df8bb
Bruce Komesu <bruce@wieck.com>
2023-07-29 23:53:40 -0500

    Handle Errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import GameDetailPage from "./pages/gameDetailPage";
 import GamePage from "./pages/gamePage";
 import Layout from "./pages/Layout";
 import Home from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <Home /> },
       { path: "games/:id", element: <GameDetailPage /> },
